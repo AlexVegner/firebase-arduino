250191f88236a24c7dd7ea73a89a896f96d8fb0f
Johan Euphrosine <proppy@google.com>
2016-04-06 14:05:54 -0700

    curl + post

--- FirebaseCurlTransport.h
+++ FirebaseCurlTransport.h
@@ -1,5 +1,5 @@
 //
-// Copyright 2015 Google Inc.
+// Copyright 2016 Google Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -14,22 +14,105 @@
 // limitations under the License.
 //
 
-// firebase-arduino is an Arduino client for Firebase.
-// It is currently limited to the ESP8266 board family.
-
 #ifndef FIREBASE_CURL_H_
 #define FIREBASE_CURL_H_
 
-#include "Firebase.h"
 #include <curl/curl.h>
 #include <string>
 #include <cassert>
+#include <cstring>
 
+namespace {
+const char kFirebaseScheme[] = "https://";
+const char kFirebasePort[] = ":443";
+const char kFirebaseExt[] = ".json";
+const char kFirebaseAuthQuery[] = "?auth=";
 size_t WriteFunction(void* contents, size_t size, size_t nmemb, void* userp) {
   static_cast<std::string*>(userp)->append(static_cast<const char*>(contents),
                                            size * nmemb);
   return size * nmemb;
 }
+void cat(char*& dst, const char *src, size_t n) {
+  memcpy(dst, src, n);
+  dst+=n;
+}
+}
+
+class FirebaseGet {
+ public:
+  FirebaseGet(const char* host, const char* auth, const char* path) : host(host), auth(auth), path(path) {
+    size_t nhost = strlen(host);
+    size_t npath = strlen(path);
+    size_t nauth = strlen(auth);
+    size_t nurl = sizeof(kFirebaseScheme)-1 + nhost + sizeof(kFirebasePort)-1 +
+        npath + sizeof(kFirebaseExt)-1 + sizeof(kFirebaseAuthQuery)-1 + nauth + 1;
+    char* p = url_ = new char[nurl]();
+    cat(p, kFirebaseScheme, sizeof(kFirebaseScheme)-1);
+    cat(p, host, nhost);
+    cat(p, kFirebasePort, sizeof(kFirebasePort)-1);
+    cat(p, path, npath);
+    cat(p, kFirebaseExt, sizeof(kFirebaseExt)-1);
+    cat(p, kFirebaseAuthQuery, sizeof(kFirebaseAuthQuery)-1);
+    cat(p, auth, nauth);
+    *p = '\0';
+  }
+  ~FirebaseGet() {
+    delete[](url_);
+  }
+  const char* url() const {
+    return url_;
+  }
+  const char* const host;
+  const char* const auth;
+  const char* const path;
+ private:
+  char* url_;
+};
+
+class FirebasePost {
+ public:
+  FirebasePost(const char* host, const char* auth, const char* path) : host(host), auth(auth), path(path) {
+    size_t nhost = strlen(host);
+    size_t npath = strlen(path);
+    size_t nauth = strlen(auth);
+    size_t nurl = sizeof(kFirebaseScheme)-1 + nhost + sizeof(kFirebasePort)-1 +
+        npath + sizeof(kFirebaseExt)-1 + sizeof(kFirebaseAuthQuery)-1 + nauth + 1;
+    char* p = url_ = new char[nurl]();
+    cat(p, kFirebaseScheme, sizeof(kFirebaseScheme)-1);
+    cat(p, host, nhost);
+    cat(p, kFirebasePort, sizeof(kFirebasePort)-1);
+    cat(p, path, npath);
+    cat(p, kFirebaseExt, sizeof(kFirebaseExt)-1);
+    cat(p, kFirebaseAuthQuery, sizeof(kFirebaseAuthQuery)-1);
+    cat(p, auth, nauth);
+    *p = '\0';
+  }
+  ~FirebasePost() {
+    delete[](url_);
+  }
+  const char* url() const {
+    return url_;
+  }
+  const char* const host;
+  const char* const auth;
+  const char* const path;
+ private:
+  char* url_;
+};
+
+class Firebase {
+ public:
+  Firebase(const char* host, const char* auth) : host(host), auth(auth) {
+  }
+  FirebaseGet get(const char* path) {
+    return FirebaseGet(host, auth, path);
+  }
+  FirebasePost post(const char* path) {
+    return FirebasePost(host, auth, path);
+  }
+  const char* const host;
+  const char* const auth;
+};
 
 class FirebaseCurlTransport {
  public:
@@ -40,22 +123,24 @@ class FirebaseCurlTransport {
   ~FirebaseCurlTransport() {
     curl_easy_cleanup(curl_);
   }
-  template<int method, size_t HOST_SIZE, size_t AUTH_SIZE, size_t PATH_SIZE>
-  int write(
-      const FirebaseRequest<method, HOST_SIZE, AUTH_SIZE, PATH_SIZE>& req) {
-    switch(method) {
-      case GET:
-        curl_easy_setopt(curl_, CURLOPT_HTTPGET, 1L);
-        break;
-      default:
-        assert(false && "unsupported method");
-        break;
-    }
-    curl_easy_setopt(curl_, CURLOPT_URL, req.url);
+
+  int write(const FirebaseGet& req) {
+    curl_easy_setopt(curl_, CURLOPT_HTTPGET, 1L);
+    curl_easy_setopt(curl_, CURLOPT_URL, req.url());
     curl_easy_setopt(curl_, CURLOPT_WRITEFUNCTION, &WriteFunction);
     curl_easy_setopt(curl_, CURLOPT_WRITEDATA, &buf_);
-    CURLcode res = curl_easy_perform(curl_);
-    return res;
+    return curl_easy_perform(curl_);
+  }
+  int write(const FirebasePost& req) {
+    curl_easy_setopt(curl_, CURLOPT_HTTPGET, 1L);
+    curl_easy_setopt(curl_, CURLOPT_URL, req.url());
+    curl_easy_setopt(curl_, CURLOPT_WRITEFUNCTION, &WriteFunction);
+    curl_easy_setopt(curl_, CURLOPT_WRITEDATA, &buf_);
+    return 0;
+  }
+  int write(const std::string& body) {
+    curl_easy_setopt(curl_, CURLOPT_POSTFIELDS, body.c_str());
+    return curl_easy_perform(curl_);
   }
   int read(std::string* out) {
     *out = buf_;
--- main.cc
+++ main.cc
@@ -1,14 +1,19 @@
 #include "FirebaseCurlTransport.h"
-#include "Firebase.h"
 
 #include <iostream>
 
-auto firebase = Firebase("proppy-iot-button.firebaseio.com", "KqfUj6MGR1SLjeudfgWdPskmukiW1Fw7d0LT4S3u");
+Firebase firebase("proppy-iot-button.firebaseio.com", "KqfUj6MGR1SLjeudfgWdPskmukiW1Fw7d0LT4S3u");
+FirebasePost postFoo = firebase.post("/foo");
+FirebaseCurlTransport curl;
 
 int main() {
-  FirebaseCurlTransport transport;
-  transport.write(firebase.get("/"));
+  curl.write(firebase.post("/foo"));
+  curl.write("{\"some\":\"data\"}");
   std::string result;
-  transport.read(&result);
+  curl.read(&result);
+  std::cout << result << std::endl;
+
+  curl.write(firebase.get("/"));
+  curl.read(&result);
   std::cout << result << std::endl;
 }
